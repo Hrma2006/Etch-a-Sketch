4b016bb6288d5e8568176360607ea9cf91dd5da2
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-17 14:00:36 +0300

    added the grid and size variables

--- main.js
+++ main.js
@@ -1,13 +1,2 @@
-const grid=document.querySelector(".grid")
-const size=document.querySelector("#size")
-console.log(size.valueAsNumber)
-function initialize(size){
-  for(let num=0;num<size;num++){
-    const div = document.createElement('div'); 
-    div.setAttribute("class","pixel")
-    grid.appendChild(div)
-  }
-}
-
-
-
+const grid = document.querySelector(".grid");
+const size = document.querySelector("#size");
