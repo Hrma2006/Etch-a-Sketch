b66e33aa7d29819030faafd2cf4299f242307299
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-17 14:01:05 +0300

    added the initialize functions

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 const grid = document.querySelector(".grid");
 const size = document.querySelector("#size");
+
+function initialize(size) {
+	for (let num = 0; num < size ** 2; num++) {
+		const div = document.createElement("div");
+		div.setAttribute("class", "pixel");
+		grid.appendChild(div);
+	}
+}
+
