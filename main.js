85efe582a031de40de80a60169584e56f09a00ef
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-17 19:45:20 +0300

    Refactor draw function and add clear function
    - Renamed the function "initialize" to "draw" for clarity.
    - Updated the draw function to clear grid before drawing new pixels.
    - Added a new clear function to remove existing pixels from the grid.
    - Added event listener on the size input to clear the grid and redraw
    pixels when the size changes.

--- main.js
+++ main.js
@@ -1,21 +1,28 @@
 const grid = document.querySelector(".grid");
 const size = document.querySelector("#size");
 
+function clear(){
+  grid.textContent = '';
+}
 
-function initialize(size) {
+function draw(size) {
 	for (let num = 0; num < size ** 2; num++) {
 		const div = document.createElement("div");
 		div.setAttribute("class", "pixel");
 		grid.appendChild(div);
 	}
+  console.log(size)
 	scale(size);
 }
 function scale(num) {
 	const pixels = document.querySelectorAll(".pixel");
-	let length = 600 / num;
+	let length = parseFloat((500 / num).toFixed(10))
 	pixels.forEach((pixel) => {
 		pixel.style.width = `${length}px`;
 		pixel.style.height = `${length}px`;
 	});
 }
-
+size.addEventListener("input",()=> {
+  clear();
+  draw(size.valueAsNumber);
+})
