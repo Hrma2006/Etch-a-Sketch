a80ad05e0055dba2482036a5486f0732a270cc75
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-17 14:01:43 +0300

    added the scale functions

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 const grid = document.querySelector(".grid");
 const size = document.querySelector("#size");
 
+
 function initialize(size) {
 	for (let num = 0; num < size ** 2; num++) {
 		const div = document.createElement("div");
 		div.setAttribute("class", "pixel");
 		grid.appendChild(div);
 	}
+	scale(size);
+}
+function scale(num) {
+	const pixels = document.querySelectorAll(".pixel");
+	let length = 600 / num;
+	pixels.forEach((pixel) => {
+		pixel.style.width = `${length}px`;
+		pixel.style.height = `${length}px`;
+	});
 }
 
