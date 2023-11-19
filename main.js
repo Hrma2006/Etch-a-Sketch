56e76738d2e28135631e17f79acaaf22e06d0b1d
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-19 17:35:16 +0300

    added some comments

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const grid = document.querySelector(".grid");
 const size = document.querySelector("#size");
 const buttons=document.querySelectorAll("button")
+let pixels=document.querySelectorAll(".pixel")
 
 
 function clear(){
   grid.textContent = '';
 }
 
+//a function to make a given number of pixels
 function makeGrid(size) {
 	for (let num = 0; num < size ** 2; num++) {
 		const div = document.createElement("div");
@@ -15,26 +17,37 @@ function makeGrid(size) {
 	}
 	grid.style.gridTemplateColumns=`repeat(${size}, ${size}fr`
 }
-let pixels=document.querySelectorAll(".pixel")
+
+// a function called on the load of the page to make it work
 function initialize(){
 	makeGrid(size.valueAsNumber);
 	pixels=document.querySelectorAll(".pixel")
 	paint()
 }
+//called the function
 initialize()
+
+//added an event listener to detect ant change to the size
 size.addEventListener("input",()=> {
   clear();
   makeGrid(size.valueAsNumber);
+	//called pixels again to update it
 	pixels=document.querySelectorAll(".pixel")
 	paint()
 })
 
+// a variable to know what to do when starting to draw
 let state=''
+
+//change the state according to the class of the pressed button
 buttons.forEach(button=>{
 	button.addEventListener("click",(e)=>{
 		state=e.target.className
 	})
 })
+
+
+// a function to paint that takes the state and a color as an input
 function paint(color,state){
 	let isMouseDown=false
 	pixels.forEach(pixel=>{
