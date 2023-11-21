b9971218ec98dee193b9f63c07a6375b6d103b6a
hema2006 <59345676+Hrma2006@users.noreply.github.com>
2023-11-21 15:11:13 +0300

    removed the paint functions

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ let pixels = document.querySelectorAll(".pixel");
 let colorSelector = document.querySelector("#color-selector");
 let selectedColor;
 let sizeViewer = document.querySelector("p");
-const colorButton=document.querySelector(".color")
-const shadeButton=document.querySelector(".shade")
-const rainbowButton=document.querySelector(".color")
-const fillButton=document.querySelector(".color")
-const clearButton=document.querySelector(".color")
+const colorButton = document.querySelector(".color");
+const shadeButton = document.querySelector(".shade");
+const rainbowButton = document.querySelector(".color");
+const fillButton = document.querySelector(".color");
+const clearButton = document.querySelector(".color");
 function clear() {
 	grid.textContent = "";
 }
@@ -32,7 +32,6 @@ function initialize() {
 	changeSizeViewer(size.value);
 	makeGrid(size.valueAsNumber);
 	pixels = document.querySelectorAll(".pixel");
-	paint();
 }
 //called the function
 initialize();
@@ -44,13 +43,10 @@ size.addEventListener("input", () => {
 	makeGrid(size.valueAsNumber);
 	//called pixels again to update it
 	pixels = document.querySelectorAll(".pixel");
-	paint();
 });
 
 // a variable to know what to do when starting to draw
 let state = "color";
-
-// a function to paint
 
 function fillPixels() {
 	pixels.forEach((pixel) => {
@@ -67,9 +63,9 @@ function generateColor() {
 	return color.join("");
 }
 
-function clearAll(){
-pixels.forEach((pixel)=>{
-	pixel.style.backgroundColor= "#ffffff"
-})
-state=''
+function clearAll() {
+	pixels.forEach((pixel) => {
+		pixel.style.backgroundColor = "#ffffff";
+	});
+	state = "";
 }
